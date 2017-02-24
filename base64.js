925c840c6a0cf76e7e876ad2471357af62d20aeb
dr.dimitru <ceo@veliov.com>
2017-02-24 15:42:39 +0300

    Speed enhancements

--- base64.js
+++ base64.js
@@ -1,13 +1,14 @@
 const root = this;
+const fromCharCode = String.fromCharCode;
 const _floor = (num) => {
   return num | 0;
 };
 
-export class base64 {
+class base64 {
   constructor(allowWebWorker, useNative) {
-    this._keyStr           = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    this._keyArr           = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', '='];
+    this._keyTab           = { 0: 52, 1: 53, 2: 54, 3: 55, 4: 56, 5: 57, 6: 58, 7: 59, 8: 60, 9: 61, A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10, L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20, V: 21, W: 22, X: 23, Y: 24, Z: 25, a: 26, b: 27, c: 28, d: 29, e: 30, f: 31, g: 32, h: 33, i: 34, j: 35, k: 36, l: 37, m: 38, n: 39, o: 40, p: 41, q: 42, r: 43, s: 44, t: 45, u: 46, v: 47, w: 48, x: 49, y: 50, z: 51, '+': 62, '/': 63, '=': 64};
     this.isNode            = (root.navigator || root.window) ? false : true;
-    this.ascii             = /^[ -~\t\n\r]+$/;
     this._allowWebWorker   = (allowWebWorker === undefined) ? true : allowWebWorker;
     this._supportWebWorker = false;
 
@@ -16,7 +17,7 @@ export class base64 {
         const _URL = root.window.URL || root.window.webkitURL || root.window.mozURL || root.window.msURL || root.window.oURL || false;
         if (root.window.Worker && root.window.Blob && _URL) {
           this._supportWebWorker = true;
-          this._webWorkerUrl = _URL.createObjectURL(new root.window.Blob(['!function(a){"use strict";var b=/^[ -~\\t\\n\\r]+$/,c="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",d=function(a){var d,e,h,i,j,k,l,b="",m=0;for(f(a)&&(a=g(a));m<a.length;)d=a.charCodeAt(m++),e=a.charCodeAt(m++),h=a.charCodeAt(m++),i=d>>2,j=(3&d)<<4|e>>4,k=(15&e)<<2|h>>6,l=63&h,isNaN(e)?k=l=64:isNaN(h)&&(l=64),b+=c.charAt(i)+c.charAt(j)+c.charAt(k)+c.charAt(l);return b},e=function(a){for(var d,e,f,g,i,j,k,b="",l=0;l<a.length;)g=c.indexOf(a.charAt(l++)),i=c.indexOf(a.charAt(l++)),j=c.indexOf(a.charAt(l++)),k=c.indexOf(a.charAt(l++)),d=g<<2|i>>4,e=(15&i)<<4|j>>2,f=(3&j)<<6|k,b+=String.fromCharCode(d),64!=j&&(b+=String.fromCharCode(e)),64!=k&&(b+=String.fromCharCode(f));return h(b)},f=function(a){return!b.test(a)},g=function(a){a=a.replace(/\\r\\n/g,"\\n");for(var b="",c=0;c<a.length;c++){var d=a.charCodeAt(c);d<128?b+=String.fromCharCode(d):d>127&&d<2048?(b+=String.fromCharCode(d>>6|192),b+=String.fromCharCode(63&d|128)):(b+=String.fromCharCode(d>>12|224),b+=String.fromCharCode(d>>6&63|128),b+=String.fromCharCode(63&d|128))}return b},h=function(a){for(var b="",c=0,d=0,e=0,f=0;c<a.length;)d=a.charCodeAt(c),d<128?(b+=String.fromCharCode(d),c++):d>191&&d<224?(e=a.charCodeAt(c+1),b+=String.fromCharCode((31&d)<<6|63&e),c+=2):(e=a.charCodeAt(c+1),f=a.charCodeAt(c+2),b+=String.fromCharCode((15&d)<<12|(63&e)<<6|63&f),c+=3);return b};a.onmessage=function(a){"encode"===a.datatype?postMessage({id:a.data.id,res:d(a.data.e)}):postMessage({id:a.data.id,res:e(a.data.e)})}}(this);'], {
+          this._webWorkerUrl = _URL.createObjectURL(new root.window.Blob([";(function(c){'use strict';var d=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/','='];var g={0:52,1:53,2:54,3:55,4:56,5:57,6:58,7:59,8:60,9:61,A:0,B:1,C:2,D:3,E:4,F:5,G:6,H:7,I:8,J:9,K:10,L:11,M:12,N:13,O:14,P:15,Q:16,R:17,S:18,T:19,U:20,V:21,W:22,X:23,Y:24,Z:25,a:26,b:27,c:28,d:29,e:30,f:31,g:32,h:33,i:34,j:35,k:36,l:37,m:38,n:39,o:40,p:41,q:42,r:43,s:44,t:45,u:46,v:47,w:48,x:49,y:50,z:51,'+':62,'/':63,'=':64};var h=String.fromCharCode;var j=function(e){var b='',n,r,i,u,a,f=0;while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}b+=d[n>>2]+d[(n&3)<<4|r>>4]+d[u]+d[a]}return b};var k=function(e){e=e.replace(/\\r\\n/g,'\\n');var a='',n,r;for(n=0;n<e.length;n++){r=e.charCodeAt(n);if(r<128){a+=h(r)}else if(r>127&&r<2048){a+=h(r>>6|192);a+=h(r&63|128)}else{a+=h(r>>12|224);a+=h(r>>6&63|128);a+=h(r&63|128)}}return a};var l=function(e){var b='',s,o,u,a,f=0;while(f<e.length){s=g[e.charAt(f++)];o=g[e.charAt(f++)];u=g[e.charAt(f++)];a=g[e.charAt(f++)];b+=h(s<<2|o>>4);if(u!=64){b+=h((o&15)<<4|u>>2)}if(a!=64){b+=h((u&3)<<6|a)}}return b};var m=function(e){var a='',n=0,r=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){a+=h(r);n++}else if(r>191&&r<224){a+=h((r&31)<<6|e.charCodeAt(n+1)&63);n+=2}else{a+=h((r&15)<<12|(e.charCodeAt(n+1)&63)<<6|e.charCodeAt(n+2)&63);n+=3}}return a};c.onmessage=function(e){if(e.data.type==='encode'){postMessage({id:e.data.id,res:j(k(e.data.e))})}else{postMessage({id:e.data.id,res:m(l(e.data.e))})}}})(this);"], {
             type: 'application/javascript'
           }));
         } else if (root.window.Worker) {
@@ -54,148 +55,151 @@ export class base64 {
       this.isNode    = false;
     }
   }
+
   _registerHandler(id, cb) {
     this._handlers[id] = cb;
   }
+
   encode(e, cb) {
-    if (e.length === 0) {
+    let res = e;
+    if (res.length === 0) {
+      cb && cb(null, '');
       return '';
     }
+
+    res = String(res);
     if (this.isNode && this.useNative) {
       if (typeof Buffer.from === 'function') {
-        return Buffer.from(e, 'utf8').toString('base64');
-      } else {
-        return new Buffer(e, 'utf8').toString('base64');
+        return Buffer.from(res, 'utf8').toString('base64');
       }
+      return new Buffer(res, 'utf8').toString('base64');
     } else if (this.useNative && !this.isNode) {
-      if (this._isUnicode(e)){
-        e = this._utf8_encode(e);
-      }
-      return root.window.btoa(e);
-    } else {
-      if (cb && this._supportWebWorker) {
-        const id = _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-        this._registerHandler(id, cb);
-        this._worker.postMessage({type: 'encode', id: id, e: e});
-      } else {
-        let t = '', n, r, i, s, o, u, a, f = 0;
-        if (this._isUnicode(e)){
-          e = this._utf8_encode(e);
-        }
-        while (f < e.length) {
-          n = e.charCodeAt(f++);
-          r = e.charCodeAt(f++);
-          i = e.charCodeAt(f++);
-          s = n >> 2;
-          o = (n & 3) << 4 | r >> 4;
-          u = (r & 15) << 2 | i >> 6;
-          a = i & 63;
-          if (isNaN(r)) {
-            u = a = 64;
-          } else if (isNaN(i)) {
-            a = 64;
-          }
-          t += this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a);
-        }
-        if (cb) {
-          cb(null, t);
-        } else {
-          return t;
-        }
-      }
+      return root.window.btoa(this._utf8Encode(res));
     }
+
+    if (cb && this._supportWebWorker) {
+      const id = this._getId();
+      this._registerHandler(id, cb);
+      this._worker.postMessage({type: 'encode', id: id, e: res});
+      return void 0;
+    }
+
+    res = this._encode(this._utf8Encode(res));
+    if (cb) {
+      cb(null, res);
+      return void 0;
+    }
+    return res;
   }
 
   decode(e, cb) {
-    if (e.length === 0) {
+    let res = e;
+    if (res.length === 0) {
+      cb && cb(null, '');
       return '';
     }
-    e = e.replace(/[^A-Za-z0-9\+\/\=]/g, '');
+
     if (this.isNode && this.useNative) {
       if (typeof Buffer.from === 'function') {
-        return Buffer.from(e, 'base64').toString('utf8');
-      } else {
-        return new Buffer(e, 'base64').toString('utf8');
+        return Buffer.from(res, 'base64').toString('utf8');
       }
+      return new Buffer(res, 'base64').toString('utf8');
     } else if (this.useNative && !this.isNode) {
-      return this._utf8_decode(root.window.atob(e));
-    } else {
-      if (cb && this._supportWebWorker) {
-        const id = _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-        this._registerHandler(id, cb);
-        this._worker.postMessage({type: 'decode', id: id, e: e});
-      } else {
-        let t = '', n, r, i, s, o, u, a, f = 0;
-        while (f < e.length) {
-          s = this._keyStr.indexOf(e.charAt(f++));
-          o = this._keyStr.indexOf(e.charAt(f++));
-          u = this._keyStr.indexOf(e.charAt(f++));
-          a = this._keyStr.indexOf(e.charAt(f++));
-          n = s << 2 | o >> 4;
-          r = (o & 15) << 4 | u >> 2;
-          i = (u & 3) << 6 | a;
-          t += String.fromCharCode(n);
-          if (u != 64) {
-            t += String.fromCharCode(r);
-          }
-          if (a != 64) {
-            t += String.fromCharCode(i);
-          }
-        }
-        t = this._utf8_decode(t);
-        if (cb) {
-          cb(null, t);
-        } else {
-          return t;
-        }
-      }
+      return this._utf8Decode(root.window.atob(res));
     }
+
+    if (cb && this._supportWebWorker) {
+      const id = this._getId();
+      this._registerHandler(id, cb);
+      this._worker.postMessage({type: 'decode', id: id, e: res});
+      return void 0;
+    }
+
+    res = this._utf8Decode(this._decode(res));
+    if (cb) {
+      cb(null, res);
+      return void 0;
+    }
+    return res;
   }
 
-  _isUnicode(e) {
-    return !this.ascii.test(e);
+  _encode(e) {
+    let res = '', n, r, i, u, a, f = 0;
+    while (f < e.length) {
+      n = e.charCodeAt(f++);
+      r = e.charCodeAt(f++);
+      i = e.charCodeAt(f++);
+      u = (r & 15) << 2 | i >> 6;
+      a = i & 63;
+      if (isNaN(r)) {
+        u = a = 64;
+      } else if (isNaN(i)) {
+        a = 64;
+      }
+      res += this._keyArr[n >> 2] + this._keyArr[(n & 3) << 4 | r >> 4] + this._keyArr[u] + this._keyArr[a];
+    }
+    return res;
   }
 
-  _utf8_encode(e) {
+  _utf8Encode(e) {
     e = e.replace(/\r\n/g, '\n');
-    let t = '';
-    for (let n = 0; n < e.length; n++) {
-      const r = e.charCodeAt(n);
+    let res = '', n, r;
+    for (n = 0; n < e.length; n++) {
+      r = e.charCodeAt(n);
       if (r < 128) {
-        t += String.fromCharCode(r);
+        res += fromCharCode(r);
       } else if (r > 127 && r < 2048) {
-        t += String.fromCharCode(r >> 6 | 192);
-        t += String.fromCharCode(r & 63 | 128);
+        res += fromCharCode(r >> 6 | 192);
+        res += fromCharCode(r & 63 | 128);
       } else {
-        t += String.fromCharCode(r >> 12 | 224);
-        t += String.fromCharCode(r >> 6 & 63 | 128);
-        t += String.fromCharCode(r & 63 | 128);
+        res += fromCharCode(r >> 12 | 224);
+        res += fromCharCode(r >> 6 & 63 | 128);
+        res += fromCharCode(r & 63 | 128);
       }
     }
-    return t;
+    return res;
   }
 
-  _utf8_decode(e) {
-    let t = '', n = 0, r = 0, c2 = 0, c3 = 0;
+  _decode(e) {
+    let res = '', s, o, u, a, f = 0;
+    while (f < e.length) {
+      s = this._keyTab[e.charAt(f++)];
+      o = this._keyTab[e.charAt(f++)];
+      u = this._keyTab[e.charAt(f++)];
+      a = this._keyTab[e.charAt(f++)];
+      res += fromCharCode(s << 2 | o >> 4);
+      if (u != 64) {
+        res += fromCharCode((o & 15) << 4 | u >> 2);
+      }
+      if (a != 64) {
+        res += fromCharCode((u & 3) << 6 | a);
+      }
+    }
+    return res;
+  }
+
+  _utf8Decode(e) {
+    let res = '', n = 0, r = 0;
     while (n < e.length) {
       r = e.charCodeAt(n);
       if (r < 128) {
-        t += String.fromCharCode(r);
+        res += fromCharCode(r);
         n++;
       } else if (r > 191 && r < 224) {
-        c2 = e.charCodeAt(n + 1);
-        t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+        res += fromCharCode((r & 31) << 6 | e.charCodeAt(n + 1) & 63);
         n += 2;
       } else {
-        c2 = e.charCodeAt(n + 1);
-        c3 = e.charCodeAt(n + 2);
-        t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+        res += fromCharCode((r & 15) << 12 | (e.charCodeAt(n + 1) & 63) << 6 | e.charCodeAt(n + 2) & 63);
         n += 3;
       }
     }
-    return t;
+    return res;
+  }
+
+  _getId() {
+    return _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + '-' + _floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   }
 }
 
 const Base64 = new base64();
-export {Base64};
+export {Base64, base64};
