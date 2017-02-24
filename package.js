496c37f0b6218f306f17942ece8b07d7293ab1c0
dr.dimitru <ceo@veliov.com>
2017-02-24 15:47:11 +0300

    v1.1.1
    - Speed enhancements
    - Fix encode in WebWorkers
    - Compatibility with `meteor@1.4.3.1`

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'ostrio:base64',
-  version: '1.1.0',
+  version: '1.1.1',
   summary: 'Efficient isomorphic Base64 implementation, with support of WebWorkers, Native code and Unicode',
   git: 'https://github.com/VeliovGroup/meteor-base64',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use('ecmascript@0.1.5');
+  api.versionsFrom('1.4');
+  api.use('ecmascript');
   api.addAssets('worker.min.js', 'client');
   api.mainModule('base64.js');
   api.export('Base64');
