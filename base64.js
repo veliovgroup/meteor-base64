4ef28243cbf70c2862b450f69d3cbacebf1dc2bf
dr.dimitru <ceo@veliov.com>
2017-12-05 08:44:41 +0300

    pre-1.3.0
     - Refactored
     - Blazing fast
     - Compatibility with latest Meteor

--- base64.js
+++ base64.js
@@ -4,24 +4,23 @@ const root = this;
 const fromCharCode = String.fromCharCode;
 
 class base64 {
-  constructor(allowWebWorker, useNative) {
-    this._keyArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', '='];
-    this._keyTab = { 0: 52, 1: 53, 2: 54, 3: 55, 4: 56, 5: 57, 6: 58, 7: 59, 8: 60, 9: 61, A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10, L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20, V: 21, W: 22, X: 23, Y: 24, Z: 25, a: 26, b: 27, c: 28, d: 29, e: 30, f: 31, g: 32, h: 33, i: 34, j: 35, k: 36, l: 37, m: 38, n: 39, o: 40, p: 41, q: 42, r: 43, s: 44, t: 45, u: 46, v: 47, w: 48, x: 49, y: 50, z: 51, '+': 62, '/': 63, '=': 64};
-    this.isNode            = (root.navigator || root.window) ? false : true;
-    this._allowWebWorker   = (allowWebWorker === undefined) ? true : allowWebWorker;
+  constructor(opts = {}) {
+    this._isNode           = (root.navigator || root.window) ? false : true;
+    this._useNative        = (opts.useNative === undefined) ? false : opts.useNative;
+    this._allowWebWorker   = (opts.allowWebWorker === undefined) ? true : opts.allowWebWorker;
+    this._ejsonCompatible  = (opts.ejsonCompatible === undefined) ? false : opts.ejsonCompatible;
     this._supportWebWorker = false;
 
     if (this._allowWebWorker) {
-      if (!this.isNode) {
+      if (!this._isNode) {
         const _URL = root.window.URL || root.window.webkitURL || root.window.mozURL || root.window.msURL || root.window.oURL || false;
         try {
           if (root.window.Worker && root.window.Blob && _URL) {
             this._supportWebWorker = true;
-            this._webWorkerUrl = _URL.createObjectURL(new root.window.Blob(["!function(){'use strict';var t=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/','='],r={0:52,1:53,2:54,3:55,4:56,5:57,6:58,7:59,8:60,9:61,A:0,B:1,C:2,D:3,E:4,F:5,G:6,H:7,I:8,J:9,K:10,L:11,M:12,N:13,O:14,P:15,Q:16,R:17,S:18,T:19,U:20,V:21,W:22,X:23,Y:24,Z:25,a:26,b:27,c:28,d:29,e:30,f:31,g:32,h:33,i:34,j:35,k:36,l:37,m:38,n:39,o:40,p:41,q:42,r:43,s:44,t:45,u:46,v:47,w:48,x:49,y:50,z:51,'+':62,'/':63,'=':64},e=String.fromCharCode,a=function(r){for(var e,a,n,o,c,d='',h=0;h<r.length;)e=r.charCodeAt(h++),o=(15&(a=r.charCodeAt(h++)))<<2|(n=r.charCodeAt(h++))>>6,c=63&n,isNaN(a)?o=c=64:isNaN(n)&&(c=64),d+=t[e>>2]+t[(3&e)<<4|a>>4]+t[o]+t[c];return d},n=function(t){var r,a,n=t.replace(/\\r\\n/g,'\\n'),o='';for(r=0;r<n.length;r++)(a=n.charCodeAt(r))<128?o+=e(a):a>127&&a<2048?(o+=e(a>>6|192),o+=e(63&a|128)):(o+=e(a>>12|224),o+=e(a>>6&63|128),o+=e(63&a|128));return o},o=function(t){for(var a,n,o,c,d='',h=0;h<t.length;)a=r[t.charAt(h++)],n=r[t.charAt(h++)],o=r[t.charAt(h++)],c=r[t.charAt(h++)],d+=e(a<<2|n>>4),64!==o&&(d+=e((15&n)<<4|o>>2)),64!==c&&(d+=e((3&o)<<6|c));return d},c=function(t){for(var r='',a=0,n=0;a<t.length;)(n=t.charCodeAt(a))<128?(r+=e(n),a++):n>191&&n<224?(r+=e((31&n)<<6|63&t.charCodeAt(a+1)),a+=2):(r+=e((15&n)<<12|(63&t.charCodeAt(a+1))<<6|63&t.charCodeAt(a+2)),a+=3);return r};addEventListener('message',function(t){'encode'===t.data.type?postMessage({id:t.data.id,res:a(n(t.data.e))}):postMessage({id:t.data.id,res:c(o(t.data.e))})})}();"], {
+            this._webWorkerUrl = _URL.createObjectURL(new root.window.Blob(["!function(){\"use strict\";var a=[\"A\",\"B\",\"C\",\"D\",\"E\",\"F\",\"G\",\"H\",\"I\",\"J\",\"K\",\"L\",\"M\",\"N\",\"O\",\"P\",\"Q\",\"R\",\"S\",\"T\",\"U\",\"V\",\"W\",\"X\",\"Y\",\"Z\",\"a\",\"b\",\"c\",\"d\",\"e\",\"f\",\"g\",\"h\",\"i\",\"j\",\"k\",\"l\",\"m\",\"n\",\"o\",\"p\",\"q\",\"r\",\"s\",\"t\",\"u\",\"v\",\"w\",\"x\",\"y\",\"z\",\"0\",\"1\",\"2\",\"3\",\"4\",\"5\",\"6\",\"7\",\"8\",\"9\",\"+\",\"/\",\"=\"],t={0:52,1:53,2:54,3:55,4:56,5:57,6:58,7:59,8:60,9:61,A:0,B:1,C:2,D:3,E:4,F:5,G:6,H:7,I:8,J:9,K:10,L:11,M:12,N:13,O:14,P:15,Q:16,R:17,S:18,T:19,U:20,V:21,W:22,X:23,Y:24,Z:25,a:26,b:27,c:28,d:29,e:30,f:31,g:32,h:33,i:34,j:35,k:36,l:37,m:38,n:39,o:40,p:41,q:42,r:43,s:44,t:45,u:46,v:47,w:48,x:49,y:50,z:51,\"+\":62,\"/\":63,\"=\":64},e=String.fromCharCode,r=!(!atob||!btoa);addEventListener(\"message\",function(n){\"encode\"===n.data.type?postMessage({id:n.data.id,res:function(t,e){var n=\"\";if(t)if(e&&r)n=btoa(t);else for(var o,d,c,i,f,h=0;h<t.length;)o=t.charCodeAt(h++),i=(15&(d=t.charCodeAt(h++)))<<2|(c=t.charCodeAt(h++))>>6,f=63&c,isNaN(d)?i=f=64:isNaN(c)&&(f=64),n+=a[o>>2]+a[(3&o)<<4|d>>4]+a[i]+a[f];return n}(function(a){var t=\"\";if(a){var r,n,o=a.replace(/\\r\\n/g,\"\\n\");for(r=0;r<o.length;r++)(n=o.charCodeAt(r))<128?t+=e(n):n>127&&n<2048?(t+=e(n>>6|192),t+=e(63&n|128)):(t+=e(n>>12|224),t+=e(n>>6&63|128),t+=e(63&n|128))}return t}(n.data.e),n.data.n)}):postMessage({id:n.data.id,type:\"decode\",res:function(a){var t=\"\";if(a)for(var r=0,n=0;r<a.length;)(n=a.charCodeAt(r))<128?(t+=e(n),r++):n>191&&n<224?(t+=e((31&n)<<6|63&a.charCodeAt(r+1)),r+=2):(t+=e((15&n)<<12|(63&a.charCodeAt(r+1))<<6|63&a.charCodeAt(r+2)),r+=3);return t}(function(a,n){var o=\"\";if(a)if(n&&r)o=atob(a);else for(var d,c,i,f,h=0;h<a.length;)d=t[a.charAt(h++)],c=t[a.charAt(h++)],i=t[a.charAt(h++)],f=t[a.charAt(h++)],o+=e(d<<2|c>>4),64!==i&&(o+=e((15&c)<<4|i>>2)),64!==f&&(o+=e((3&i)<<6|f));return o}(n.data.e,n.data.n))})})}();"], {
               type: 'application/javascript'
             }));
-          } else 
-          if (root.window.Worker) {
+          } else if (root.window.Worker) {
             this._supportWebWorker = true;
             this._webWorkerUrl = Meteor.absoluteUrl('packages/ostrio_base64/worker.min.js');
           } else {
@@ -33,7 +32,7 @@ class base64 {
             this._handlers = {};
             this._worker.onmessage = (e) => {
               if (this._handlers[e.data.id]) {
-                this._handlers[e.data.id](null, e.data.res);
+                this._handlers[e.data.id](void 0, ((this._ejsonCompatible && e.data.type === 'decode') ? base64.str2uint8(e.data.res) : e.data.res));
                 delete this._handlers[e.data.id];
               }
             };
@@ -51,14 +50,18 @@ class base64 {
       }
     }
 
-    if (useNative && !this.isNode) {
-      this.useNative = !!(root.window.atob && root.window.btoa);
-    } else if (useNative && this.isNode) {
-      this.useNative = true;
+    if (this._useNative && !this._isNode) {
+      this._useNative = !!(root.window.atob && root.window.btoa);
+    } else if (this._useNative && this._isNode) {
+      this._useNative = true;
     } else {
-      this.useNative = false;
-      this.isNode    = false;
+      this._useNative = false;
+      this._isNode    = false;
     }
+  }
+
+  get newBinary() {
+    return base64.newBinary;
   }
 
   _registerHandler(id, cb) {
@@ -68,30 +71,32 @@ class base64 {
   encode(e, cb) {
     let res = e;
     if (res.length === 0) {
-      cb && cb(null, '');
-      return '';
-    }
-
-    res = String(res);
-    if (this.isNode && this.useNative) {
-      if (typeof Buffer.from === 'function') {
-        return Buffer.from(res, 'utf8').toString('base64');
+      res = '';
+    } else {
+      if (this._ejsonCompatible && typeof res !== 'string') {
+        res = String.fromCharCode.apply(null, res);
       }
-      return new Buffer(res, 'utf8').toString('base64');
-    } else if (this.useNative && !this.isNode) {
-      return root.window.btoa(this._utf8Encode(res));
+
+      if (this._isNode && this._useNative) {
+        if (typeof Buffer.from === 'function') {
+          res = Buffer.from(res, 'utf8').toString('base64');
+        } else {
+          res = new Buffer(res, 'utf8').toString('base64');
+        }
+      } else if (cb && !this._isNode && this._supportWebWorker) {
+        const id = base64._getId();
+        this._registerHandler(id, cb);
+        this._worker.postMessage({type: 'encode', n: this._useNative, id: id, e: res});
+        return void 0;
+      } else if (this._useNative && !this._isNode) {
+        res = root.window.btoa(base64._utf8Encode(res));
+      } else {
+        res = base64._encode(base64._utf8Encode(res));
+      }
     }
 
-    if (cb && this._supportWebWorker) {
-      const id = this._getId();
-      this._registerHandler(id, cb);
-      this._worker.postMessage({type: 'encode', id: id, e: res});
-      return void 0;
-    }
-
-    res = this._encode(this._utf8Encode(res));
     if (cb) {
-      cb(null, res);
+      cb(void 0, res);
       return void 0;
     }
     return res;
@@ -100,35 +105,46 @@ class base64 {
   decode(e, cb) {
     let res = e;
     if (res.length === 0) {
-      cb && cb(null, '');
-      return '';
-    }
-
-    if (this.isNode && this.useNative) {
-      if (typeof Buffer.from === 'function') {
-        return Buffer.from(res, 'base64').toString('utf8');
+      res = '';
+    } else {
+      if (this._ejsonCompatible && typeof res !== 'string') {
+        res = String.fromCharCode.apply(null, res);
       }
-      return new Buffer(res, 'base64').toString('utf8');
-    } else if (this.useNative && !this.isNode) {
-      return this._utf8Decode(root.window.atob(res));
+
+      if (this._isNode && this._useNative) {
+        if (typeof Buffer.from === 'function') {
+          res = Buffer.from(res, 'base64').toString('utf8');
+        } else {
+          res = new Buffer(res, 'base64').toString('utf8');
+        }
+      } else if (cb && !this._isNode && this._supportWebWorker) {
+        const id = base64._getId();
+        this._registerHandler(id, cb);
+        this._worker.postMessage({type: 'decode', n: this._useNative, id: id, e: res});
+        return void 0;
+      } else if (this._useNative && !this._isNode) {
+        res = base64._utf8Decode(root.window.atob(res));
+      } else {
+        res = base64._utf8Decode(base64._decode(res));
+      }
     }
 
-    if (cb && this._supportWebWorker) {
-      const id = this._getId();
-      this._registerHandler(id, cb);
-      this._worker.postMessage({type: 'decode', id: id, e: res});
-      return void 0;
+    if (this._ejsonCompatible) {
+      res = base64.str2uint8(res);
     }
 
-    res = this._utf8Decode(this._decode(res));
     if (cb) {
-      cb(null, res);
+      cb(void 0, res);
       return void 0;
     }
     return res;
   }
 
-  _encode(e) {
+  static _encode(e) {
+    if (!e) {
+      return '';
+    }
+
     let res = '';
     let n;
     let r;
@@ -147,12 +163,16 @@ class base64 {
       } else if (isNaN(i)) {
         a = 64;
       }
-      res += this._keyArr[n >> 2] + this._keyArr[(n & 3) << 4 | r >> 4] + this._keyArr[u] + this._keyArr[a];
+      res += base64._keyArr[n >> 2] + base64._keyArr[(n & 3) << 4 | r >> 4] + base64._keyArr[u] + base64._keyArr[a];
     }
     return res;
   }
 
-  _utf8Encode(_e) {
+  static _utf8Encode(_e) {
+    if (!_e) {
+      return '';
+    }
+
     const e = _e.replace(/\r\n/g, '\n');
     let res = '';
     let n;
@@ -173,7 +193,11 @@ class base64 {
     return res;
   }
 
-  _decode(e) {
+  static _decode(e) {
+    if (!e) {
+      return '';
+    }
+
     let res = '';
     let s;
     let o;
@@ -181,10 +205,10 @@ class base64 {
     let a;
     let f = 0;
     while (f < e.length) {
-      s = this._keyTab[e.charAt(f++)];
-      o = this._keyTab[e.charAt(f++)];
-      u = this._keyTab[e.charAt(f++)];
-      a = this._keyTab[e.charAt(f++)];
+      s = base64._keyTab[e.charAt(f++)];
+      o = base64._keyTab[e.charAt(f++)];
+      u = base64._keyTab[e.charAt(f++)];
+      a = base64._keyTab[e.charAt(f++)];
       res += fromCharCode(s << 2 | o >> 4);
       if (u !== 64) {
         res += fromCharCode((o & 15) << 4 | u >> 2);
@@ -196,7 +220,11 @@ class base64 {
     return res;
   }
 
-  _utf8Decode(e) {
+  static _utf8Decode(e) {
+    if (!e) {
+      return '';
+    }
+
     let res = '';
     let n = 0;
     let r = 0;
@@ -216,9 +244,45 @@ class base64 {
     return res;
   }
 
-  _getId() {
+  static _getId() {
     return Math.random().toString(36).slice(2, 18);
   }
+
+  static newUint8Array(len) {
+    if (typeof Uint8Array === 'undefined') {
+      const ret = [];
+      for (let i = 0; i < len; i++) {
+        ret.push(0);
+      }
+      ret.$Uint8ArrayPolyfill = true;
+      return ret;
+    }
+    return new Uint8Array(len);
+  }
+
+  static newBinary(len) {
+    if (typeof Uint8Array === 'undefined' || typeof ArrayBuffer === 'undefined') {
+      const ret = [];
+      for (let i = 0; i < len; i++) {
+        ret.push(0);
+      }
+      ret.$Uint8ArrayPolyfill = true;
+      return ret;
+    }
+    return new Uint8Array(new ArrayBuffer(len));
+  }
+
+  static str2uint8(str) {
+    const uint = base64.newUint8Array(str.length);
+
+    for(let i = 0; i < str.length; ++i){
+      uint[i] = str.charCodeAt(i);
+    }
+    return uint;
+  }
+
+  static _keyArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', '='];
+  static _keyTab = { 0: 52, 1: 53, 2: 54, 3: 55, 4: 56, 5: 57, 6: 58, 7: 59, 8: 60, 9: 61, A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10, L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20, V: 21, W: 22, X: 23, Y: 24, Z: 25, a: 26, b: 27, c: 28, d: 29, e: 30, f: 31, g: 32, h: 33, i: 34, j: 35, k: 36, l: 37, m: 38, n: 39, o: 40, p: 41, q: 42, r: 43, s: 44, t: 45, u: 46, v: 47, w: 48, x: 49, y: 50, z: 51, '+': 62, '/': 63, '=': 64};
 }
 
 const Base64 = new base64();
