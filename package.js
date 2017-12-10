48347c31f091c589ae422e429a4ab29c07c16567
dr.dimitru <ceo@veliov.com>
2017-12-10 06:03:27 +0300

    v2.0.0
     - New API, see updated docs
     - Now `ostrio:base64`, can replace default `base64` package, see
    updated docs
     - Compatibility with `meteor@1.6.0.1`

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
   name: 'ostrio:base64',
-  version: '1.2.3',
+  version: '2.0.0',
   summary: 'Efficient isomorphic Base64 implementation, with support of WebWorkers, Native code and Unicode',
   git: 'https://github.com/VeliovGroup/meteor-base64',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4');
+  api.versionsFrom('1.6');
   api.use('ecmascript');
   api.addAssets('worker.min.js', 'client');
   api.mainModule('base64.js');
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'ecmascript']);
+  api.use(['tinytest', 'ecmascript', 'ejson', 'underscore']);
   api.use('ostrio:base64');
   api.addFiles('base64-tests.js');
 });
